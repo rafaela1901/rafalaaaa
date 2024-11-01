fc983eab0d71b417070953616e6785f50c27d7fc
rafaela1901 <161044359+rafaela1901@users.noreply.github.com>
2024-11-01 10:55:54 -0300

    Add files via upload

---
+++ desenvolvendo uma ferramenta rafa/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Oi em ingles é bye'
+)
